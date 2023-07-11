f4fef374da546ee9c2d0f101c363aa0ae5010c45
Juan Cruz Miranda <juanmir227@gmail.com>
2023-07-11 16:42:42 -0300

    wawa

--- functions/k_distribution_fit.py
+++ functions/k_distribution_fit.py
@@ -1,7 +1,5 @@
 from algosdk.v2client import algod
 import pickle
-import os
-import sys
 from get_blocks import GetBlockInfo
 from acquire_txns import join_txns
 from txns_dataframe import make_df
@@ -41,13 +39,13 @@ error = np.sqrt(n)
 # print(n, bins)
 
 values = n/np.sum(n)
-print(bins)
+# print(bins)
 density_error = error/np.sum(n)
 indexes = []
 new_values = []
 new_bins = []
 new_density_error = []
-print(values)
+# print(values)
 for i,el in enumerate(values):
     if el != 0:
         new_values.append(values[i])
@@ -58,84 +56,48 @@ bins = new_bins
 values = new_values
 density_error = new_density_error
 
-# for i in range(len(values)):
-#     if values[i] == 0:
-#         values[i] = 1e-8
-# for i in range(len(bins)):
-#         if bins[i] == 0:
-#             bins[i] = 1e-8
-# for i in range(len(density_error)):
-#         if density_error[i] == 0:
-#             density_error[i] = 1e-8
-# print(values, density_error)
-# for i in range(len(n)):
-#     if n[i] == 0:
-#         n[i] = 1e-8
-# for i in range(len(bins)):
-#         if bins[i] == 0:
-#             bins[i] = 1e-8
-# for i in range(len(error)):
-#         if error[i] == 0:
-#             error[i] = 1e-8
-
-def model(x,a,gamma):
-    return a*x**gamma
 
 # threshold = 10
 # bins = bins[:threshold]
 # values = values[:threshold]
 # density_error = density_error[:threshold]
 
-params, pcov = curve_fit(model, xdata =bins, ydata = values, p0 = (0.47, -1), sigma = density_error)
-print(params)
-plt.errorbar(bins,values,yerr = density_error, label = 'data')
+def model(x,A,gamma):
+    return A*x**gamma
+#Aca genero el ajuste por el modelo model
+params, pcov = curve_fit(model, xdata =bins, ydata = values, p0 = (0.6,-3), sigma = density_error)
+print(f'Gamma value:{params[1]}')
 prediction = model(bins, params[0], params[1])
-plt.plot(bins, prediction, label = 'fit')
-plt.xscale('log')
-plt.yscale('log')
-plt.legend()
-plt.title('Ajuste por X^gamma')
-plt.show()
 
-# def new_model(x,a,gamma):
-#      return a*np.exp(x*gamma)
+bara_bins = list(range(1,15))
+print(bara_bins)
+A = values[0]
+exponent = -3
+bara_values = [A*x**exponent for x in bara_bins]
 
-# new_params,pcov = curve_fit(new_model,xdata =bins, ydata = values, p0 = (1.3, -1), sigma = density_error)
-# print(new_params)
-# plt.errorbar(bins,values,yerr = density_error, label = 'data')
-# plt.plot(bins, new_model(bins, new_params[0], new_params[1]), label = 'fit')
-# plt.title('Ajuste por e^(x*gamma)')
-# # plt.plot(bins, new_model(bins, 1.5, -0.7), color = 'pink')
-# plt.show()
-
-# #Voy a implementar una distribucion de poisson
-# def fit_poisson(k,mu):
-#      return poisson.pmf(k-1,mu)
-
-# poisson_params, pcov = curve_fit(fit_poisson, xdata = bins, ydata = values, sigma = density_error)
-# print(poisson_params)
-# plt.errorbar(bins,values,yerr=density_error, label = 'data')
-# plt.plot(bins, fit_poisson(bins, poisson_params[0]), label = 'fit')
-# plt.title('Ajuste por Poisson')
-# plt.show()
-
+print(values[0])
 mean_k = np.mean(np.array(degree_list))
-def my_distro(x, A, alfa, mu):
+def stretched_exponential(x, A,alfa, mu):
     mean_x = mean_k
     return A*x**(-alfa)*np.exp((-2*mu)/(mean_x*(1-alfa))*x**(1-alfa))
-
-stretch_exp, pcov = curve_fit(my_distro, xdata = bins, ydata = values, p0= (0.7,0.5, 1), sigma = density_error)
-plt.errorbar(bins,values,yerr=density_error, label = 'data')
-y = my_distro(bins, stretch_exp[0], stretch_exp[1], stretch_exp[2])
-print(stretch_exp)
+#aca genero el ajuste por el modelo stretched_eponential
+stretch_exp, pcov = curve_fit(stretched_exponential, xdata = bins, ydata = values, p0= (0.7,0.5, 1), sigma = density_error)
+print(f'Alfa Value:{stretch_exp[1]}')
+y = stretched_exponential(bins, stretch_exp[0], stretch_exp[1], stretch_exp[2])
+plt.errorbar(bins,values,yerr = density_error, label = 'data')
+plt.plot(bins, prediction, label = 'fit')
 plt.plot(bins,y, label = 'stretched exp')
+plt.plot(bara_bins, bara_values, label = 'barabasi')
 plt.yscale('log')
 plt.xscale('log')
-plt.title('Ajuste por Sub Preferential')
+plt.legend()
+plt.title('Ajustes')
+# plt.xlim(0,100)
 plt.show()
 
-print(r2_score(values,prediction))
-print(r2_score(values, y))
+print(f'R_2 Power Law:{r2_score(values,prediction)}')
+print(f'R_2 Stretched Exponential:{r2_score(values, y)}')
+
 
 # G_barabasi = nx.barabasi_albert_graph(970,1)
 # temp_barabasi = list(G_barabasi.degree())
